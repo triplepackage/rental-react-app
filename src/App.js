7e54c5af06b7a602e369f41a9e748301dfd86fce
John <admin@tests-MBP-3.fios-router.home>
2018-09-25 16:03:41 -0400

    Changes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactTable from "react-table";
 import 'react-table/react-table.css'
-import {Doughnut} from 'react-chartjs-2';
+import {HorizontalBar} from 'react-chartjs-2';
+import { Navbar, Nav, NavItem, Glyphicon } from 'react-bootstrap';
 import _ from "lodash";
 
 class App extends Component {
@@ -9,54 +10,49 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data: [],
+      rentalData: [],
       groupedData: [],
-      characterData: [],
+      chartData : []
     };
   }
-
-
 
   componentDidMount() {
     fetch('http://127.0.0.1:8080/api/rentals/city/nottingham')
     .then(response => response.json())
-    .then(data => this.setState({ data }));
+    .then(data => this.setState({ rentalData: data }));
 
     fetch('http://127.0.0.1:8080/api/rentals/groupedby/city')
     .then(response => response.json())
     .then(groupedData => {
       const characters = groupedData;
-      let characterData = [];
+      let chartData = [];
+      let chartLabel = [];
+
       characters.forEach(function(character) {
-        characterData.push([character.stat, parseInt(character.count)]);
+        if(character.count > 100){
+        chartData.push([parseInt(character.count)]);
+        chartLabel.push([character.stat]);
+      }
       });
 
-      this.setState({characterData})
+      this.setState({chartData: {
+    labels: chartLabel,
+    datasets: [
+      {
+        label: 'Rentals Per County',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: chartData
+      }
+    ]
+  }});
     });
   }
 
   render() {
-    const chartData = {
-    	labels: [
-    		'Red',
-    		'Green',
-    		'Yellow'
-    	],
-    	datasets: [{
-    		data: [300, 50, 100],
-    		backgroundColor: [
-    		'#FF6384',
-    		'#36A2EB',
-    		'#FFCE56'
-    		],
-    		hoverBackgroundColor: [
-    		'#FF6384',
-    		'#36A2EB',
-    		'#FFCE56'
-    		]
-    	}]
-    };
-
     const columns = [{
         id: 'streetNumber',
         Header: 'Street Number',
@@ -89,18 +85,52 @@ class App extends Component {
       }];
 
        return (
+        <div>
           <div>
+          <Navbar fixedTop>
+                 <Navbar.Header>
+                   <Navbar.Brand>
+                     <a href="/">
+                       Baltimore County Rental
+                     </a>
+                   </Navbar.Brand>
+                   <Navbar.Toggle />
+                 </Navbar.Header>
+                 <Navbar.Collapse>
+                   <Nav pullRight>
+                     <NavItem
+                       eventKey={1}
+                       href="#">
+                       Home
+                     </NavItem>
+                     <NavItem
+                       eventKey={2}
+                       href="#">
+                       Shop
+                     </NavItem>
+                     <NavItem
+                       eventKey={3}
+                       href="#">
+                       <Glyphicon glyph="shopping-cart" />
+                       {' Cart'}
+                     </NavItem>
+                   </Nav>
+                 </Navbar.Collapse>
+               </Navbar>
+          </div>
+          <div id="content">
             <div><ReactTable
-                  data={this.state.data}
+                  data={this.state.rentalData}
                   columns={columns}
                   className="-striped -highlight"
                 />
             </div>
             <div>
-               <h2>Doughnut Example</h2>
-               <Doughnut data={chartData} />
+               <h2>Rentals by County</h2>
+               <HorizontalBar data={this.state.chartData} />
              </div>
-          </div>
+         </div>
+        </div>
        )
     }
 }
